81403757c203eee7cde70d3c2dbcd0839c74341e
JoyWambui <JoyWambui@github.com>
2021-03-06 14:42:32 +0300

    add code to get value of size chosen.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+function Pizza(size, crust){
+  this.pizzaSize = size;
+  this.crustType = crust;
+  this.cheeses = [];
+  this.meats = [];
+  this.vegetables = [];
+  this.sauces = [];
+}
 $(document).ready(function(){
-
+  console.log("ready");
+ $("#pizzaForm").submit(function(event){
+   event.preventDefault();
+   let selectSize = document.getElementById("sizeOptions").value;
+   console.log(selectSize);
+ });
 });
