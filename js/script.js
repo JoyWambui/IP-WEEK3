25927dd52b45efaae6ca5e4e039757e7e9163e22
JoyWambui <JoyWambui@github.com>
2021-03-06 16:33:57 +0300

    add code to access selected cheeses

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
-function Pizza(size, crust){
-  this.pizzaSize = size;
-  this.crustType = crust;
+function Pizza(pizzaSize, crustType){
+  this.pizzaSize = pizzaSize;
+  this.crustType = crustType;
   this.cheeses = [];
   this.meats = [];
   this.vegetables = [];
   this.sauces = [];
+
+  Pizza.prototype.addCheese = function(cheese){
+    return this.cheeses.push(cheese);
+  }
 }
 $(document).ready(function(){
   console.log("ready");
  $("#pizzaForm").submit(function(event){
    event.preventDefault();
-   let selectSize = document.getElementById("sizeOptions").value;
-   console.log(selectSize);
-   let selectCrust = document.getElementById("crustOptions").value;
-   console.log(selectCrust);
- });
+
+    let pizzaSize = document.getElementById("sizeOptions").value;
+    console.log(pizzaSize);
+    let crustType = document.getElementById("crustOptions").value;
+    console.log(crustType);
+    let newPizza = new Pizza(pizzaSize, crustType);
+    console.log(newPizza);
+    $(".cheeseSelection").each(function(){
+      let cheeses = []
+      let selectCheese = document.getElementsByName("cheese");
+      for(var i = 0; i < selectCheese.length; i++){
+        if(selectCheese[i].checked){
+          cheeses.push(selectCheese[i].value)
+        }
+      }
+      for(var i = 0; i < cheeses.length; i++){
+        newPizza.addCheese(cheeses[i])
+      }
+      //check cheese function
+        console.log(cheeses);
+    });
+  });
 });
